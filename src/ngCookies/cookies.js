57b2a501d38f25d875b3a2f7050dde96352f20e6
Crhistian Ramirez <cramirez@four51.com>
2020-01-25 15:43:30 -0600

    docs($cookies): support samesite option "none"
    Browsers support option "none" which means don't block cookies on cross-origin requests.

--- src/ngCookies/cookies.js
+++ src/ngCookies/cookies.js
@@ -38,8 +38,8 @@ angular.module('ngCookies', ['ng']).
      *   or a Date object indicating the exact date/time this cookie will expire.
      * - **secure** - `{boolean}` - If `true`, then the cookie will only be available through a
      *   secured connection.
-     * - **samesite** - `{string}` - prevents the browser from sending the cookie along with cross-site requests.
-     *   Accepts the values `lax` and `strict`. See the [OWASP Wiki](https://www.owasp.org/index.php/SameSite)
+     * - **samesite** - `{string}` - defines protocol for sending cookies along with cross-site requests.
+     *   Accepts the values `strict`, `lax`, and `none`. See the [OWASP Wiki](https://www.owasp.org/index.php/SameSite)
      *   for more info. Note that as of May 2018, not all browsers support `SameSite`,
      *   so it cannot be used as a single measure against Cross-Site-Request-Forgery (CSRF) attacks.
      *
